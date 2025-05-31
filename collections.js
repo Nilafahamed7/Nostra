da6c7e5050f628c4ecd9a730ab9a57edb2654a80
nilafahamed7 <nilafiphone@gmail.com>
2025-05-31 14:58:28 +0530

    changes made

--- collections.js
+++ collections.js
@@ -1,107 +1,97 @@
-import {products} from "./products.js";
+import { products } from "./products.js";
 
-var offer=document.querySelector(".offer")
-var offerclose = document.getElementById("offer-close")
+// --- Offer Banner ---
+var offer = document.querySelector(".offer");
+var offerclose = document.getElementById("offer-close");
 
-offerclose.addEventListener("click",function(){
-    offer.style.display = "none"
-})
+offerclose.addEventListener("click", function () {
+  offer.style.display = "none";
+});
 
-//side navbar
-var sidenavmenu=document.getElementById("side-navbar-active")
-var sidenavbar = document.querySelector(".side-navbar")
-sidenavmenu.addEventListener("click",function(){
-    sidenavbar.style.marginLeft="0px"
-})
-// var sidenavbar = document.querySelector(".side-navbar")
-var sidenavbarclose = document.getElementById("side-navbar-close")
-sidenavbarclose.addEventListener("click",function(){
-sidenavbar.style.marginLeft = "-60%"
-})
+// --- Side Navbar ---
+var sidenavmenu = document.getElementById("side-navbar-active");
+var sidenavbar = document.querySelector(".side-navbar");
 
-//product
+sidenavmenu.addEventListener("click", function () {
+  sidenavbar.style.marginLeft = "0px";
+});
 
-var container=document.querySelector(".products")
-products.forEach((product)=>{
-    var createItem = document.createElement("div")
-    createItem.classList.add("product")
-    createItem.innerHTML=` <img style="width: 20vw;" src="images/${product.src}">
-    <h1>${product.name}</h1>
+var sidenavbarclose = document.getElementById("side-navbar-close");
+
+sidenavbarclose.addEventListener("click", function () {
+  sidenavbar.style.marginLeft = "-60%";
+});
+
+// --- Products Render ---
+var container = document.querySelector(".products");
+
+products.forEach((product) => {
+  var createItem = document.createElement("div");
+  createItem.classList.add("product");
+  createItem.innerHTML = `
+    <img style="width: 20vw;" src="images/${product.src}">
+    <h1 class="product-title">${product.name}</h1>
     <p>₹${product.price}</p>
-    <tags style="visibility:hidden;">${product.tags}</tags>`
+    <tags style="visibility:hidden;">${product.tags}</tags>`;
+  container.append(createItem);
+});
 
-    container.append(createItem)
-})
+// --- Tag Filter ---
+var filterList = [];
+var tags = document.getElementsByName("tags");
 
+tags.forEach((tag) => {
+  tag.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      filterList.push(e.target.value);
+    } else {
+      filterList = filterList.filter((item) => item !== e.target.value);
+    }
+    update();
+  });
+});
 
-var filterList =[]
-var tags = document.getElementsByName("tags")
-console.log(tags)
+// --- Search Input ---
+var searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("keyup", function () {
+  update();
+});
 
-tags.forEach((tag)=>{
-    tag.addEventListener("change",(e)=>{
+// --- Update Function (Tag + Search Filtering) ---
+function update() {
+  var searchTerm = searchInput.value.toLowerCase();
+  var productList = document.querySelectorAll(".product");
 
-        if(e.target.checked)
-        {
-        filterList.push(e.target.value)
-        console.log(filterList)
-        update()
-        
+  productList.forEach((product) => {
+    var matchesTag = false;
+    var matchesSearch = false;
+
+    // Get product tags
+    var tempTags = product.querySelector("tags").innerHTML.split(",");
+
+    // Get product title
+    var productTitle = product.querySelector(".product-title").textContent.toLowerCase();
+
+    // Check tag match
+    if (filterList.length === 0) {
+      matchesTag = true;
+    } else {
+      for (let tag of filterList) {
+        if (tempTags.includes(tag)) {
+          matchesTag = true;
+          break;
         }
-        else{
-            filterList = filterList.filter(item => item !== e.target.value);
-            update()
+      }
+    }
 
-        }
+    // Check search match
+    matchesSearch = productTitle.includes(searchTerm);
 
-        
-       
-    })
-})
-
-// var searchInput = document.getElementById("searchInput")
-// searchInput.addEventListener("keyup",function(){
-//     update()
-// })
-
-function update()
-{
-   
-    var productList = document.querySelectorAll(".product")
-    for(var i=0;i<productList.length;i++){
-        var check = false
-        var product=productList[i]
-        console.log(product)
-        var temp=product.querySelector("tags").innerHTML
-       
-        console.log("elemen"+temp)
-        
-
-        const tempFilterArray = temp.split(',');
-        
-        console.log("tempfilterarray"+tempFilterArray)
-        console.log("filterlist"+filterList)
-       
-            filterList.forEach((j)=>{
-                tempFilterArray.forEach((i)=>{
-                if(j==i)
-                {
-                    check=true
-                }
-            })
-        })
-
-
-        if(!check && filterList.length>0)
-        {
-            product.style.display="none"
-        }
-        else{
-            product.style.display="block"
-        }
-
-        
-    };
-
-
+    // Show or hide product based on both
+    if (matchesTag && matchesSearch) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
 }
