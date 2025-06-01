cdfc5b46243f6b189d742022c3f8b94c80f2d2ac
nilafahamed7 <nilafiphone@gmail.com>
2025-06-01 10:06:19 +0530

    slider updated

--- script.js
+++ script.js
@@ -18,5 +18,55 @@ sidenavbarclose.addEventListener("click",function(){
 sidenavbar.style.marginLeft = "-60%"
 })
 
+//slider
+var sliderleftbutton = document.getElementById("slider-left-activate")
+var sliderrightbutton =document.getElementById("slider-right-activate")
+var sliderimage= document.querySelector(".slider-image-container")
+var slidermargin=0
+
+console.log(sliderleftbutton)
+
+sliderrightbutton.addEventListener("click",
+
+function(){
+    slidermargin=slidermargin+100
+
+    if(slidermargin>200)
+    {
+        slidermargin=0
+        sliderimage.style.marginLeft=0;
+    }
+    else{
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+   
+
+}
+)
+
+sliderleftbutton.addEventListener("click",
+
+function(){
+
+    if(slidermargin==0)
+    {
+       
+        slidermargin=200
+        sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    else{
+
+        slidermargin=slidermargin-100
+    sliderimage.style.marginLeft="-"+slidermargin+"vw";
+    }
+    
+    
+}
+)
+
+
+
+
 
 
